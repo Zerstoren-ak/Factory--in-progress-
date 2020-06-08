7680165e3d0cf00c454366cf5615749e7d01a8c2
Andrey <firebird_07@mail.ru>
2020-06-09 02:50:00 +0300

    Webpack config, SCSS familiarization, header + menu,

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+import Swiper from 'swiper';
+import 'swiper/css/swiper.css';
+import * as SliderSettings from './swiper-settings';
+import '@/css/styles.scss';
+import logo from '@/img/logo.png';
+// import '@/css/scss.scss';
+
+
+//// Swiper set & initialization + import all test (* as)
+// const Slider = document.getElementById(`slider`);
+// SliderSettings.addSliderClasses(Slider, `container-polygon`, `swiper-slide`);
+// const sw = new Swiper('.swiper-container', SliderSettings.SWSettings);
+////
+
+///jQuery test
+// $('.logo').click(function() {$(this).toggleClass('border')});
+////
+
+
+////Single import test
+// import {testFunction} from "./analytics";
+// testFunction();
+////
+
---
+++ webpack.config.js
@@ -0,0 +1,195 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebPackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Autoprefixer = require('autoprefixer');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const DevMode = process.env.NODE_ENV === 'development';
+const ProdMode = !DevMode;
+
+console.log('DevelopmentMode: ', DevMode);
+console.log('ProductionMode: ', ProdMode);
+
+function optimization() {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    };
+
+    if (ProdMode) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+}
+
+function fileName(extention) {
+    if (DevMode){
+        return `[name].${extention}`
+    } else {
+        return `[name].[hash].${extention}`
+    }
+}
+
+// function addPlugin(newPlugin) {
+//     const prodPlugin = [];
+//
+//     if (ProdMode && newPlugin) {
+//         prodPlugin.push(newPlugin);
+//     }
+//
+//     return prodPlugin;
+// }
+
+function cssLoaders(extra) {
+    let currentMode = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                hmr: DevMode,
+                reloadAll: true
+            }
+        },
+        'css-loader',
+    ];
+    const productionMode = {loader: 'postcss-loader', options: {plugins: [new Autoprefixer()]}};
+    if (ProdMode) {
+        currentMode = [...currentMode, productionMode]
+    }
+    if (extra) {
+        currentMode.push(extra)
+    }
+
+    return currentMode;
+}
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    entry: {
+        main: ['@babel/polyfill', './js/app.js'],
+        // modernizr: './js/modernizr-webp.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: fileName('js')
+    },
+    // watch: false,
+    // watchOptions: {
+    //     aggregateTimeout: 100
+    // },
+    devtool: DevMode ? 'source-map' : '',
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: DevMode
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: ProdMode
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebPackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/js/modernizr-webp.js'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: fileName('css')
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+
+            },
+            {
+                test:/\.(jpg|png|svg|gif)$/,
+                exclude: /webfonts/,
+                use: [
+                    {
+                    loader: 'file-loader',
+                    options: {
+                        name: fileName('[ext]'),
+                        outputPath: 'img'
+                    }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|svg)$/,
+                exclude: /images/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: fileName('[ext]'),
+                            outputPath: 'webfonts'
+                        }
+                    }
+                ],
+
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            }
+        ],
+    },
+};
+
